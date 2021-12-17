8c939c96d81a68851aac19314bf93882afa29940
Rafael Zuolo Coppini Lima <rafaelzcl@gmail.com>
2021-12-16 21:25:02 -0300

    Day 16 part 1 is working

--- Day16/Day16_1.java
+++ Day16/Day16_1.java
@@ -1,13 +1,76 @@
 import java.util.*;
 public class Day16_1{
     
+    static int pos = 0;
+    static String rawBinary = "";
+    static final long literalValue = 4;
 
-    static final int literalValue = 4;
-    static final int aaa = 0;
+    public static long parseSubPack(int subPackNum) {
+        long versionSum = 0;
+        for (int i = 0; i < subPackNum; i++) {
+            versionSum += parsePack();
+        }
+        return versionSum;
+    }
 
+    public static long parseLengthPack(int totalLength) {
+        long versionSum = 0;
+        int currentPos = pos;
+        while (pos < currentPos + totalLength) {
+            versionSum += parsePack();
+        }
+        return versionSum;
+    }
+
+    public static long parsePack() {
+        int version = Integer.parseInt(rawBinary.substring(pos,pos+3), 2);
+        pos += 3;
+        int typeID  = Integer.parseInt(rawBinary.substring(pos,pos+3), 2);
+        pos += 3;
+ 
+        System.out.println("version "+version);
+        System.out.println("ID "+typeID);
+
+        if (typeID == literalValue) {
+            String val = "";
+            while(rawBinary.charAt(pos) == '1') {
+                pos +=1;
+                val = val.concat(rawBinary.substring(pos, pos+4));
+                //System.out.println(val);
+                pos +=4;
+            }
+            pos +=1;
+            val = val.concat(rawBinary.substring(pos, pos+4));
+            pos +=4;
+            System.out.println("Parsed literal of val "
+                                +Long.parseLong(val,2));
+            return version;
+//            return Long.parseLong(val, 2);
+        }
+        else {
+            if (rawBinary.charAt(pos) == '1') {
+                pos++;
+                int subPackNum 
+                    = Integer.parseInt(rawBinary.substring(pos, pos+11), 2);
+                pos += 11;
+                System.out.println("Parsing "+subPackNum+" subpacks");
+                version += parseSubPack(subPackNum);// get the subpack sum
+                System.out.println("finished "+subPackNum+" subpacks");
+            } else {
+                pos++;
+                int totalLength 
+                    = Integer.parseInt(rawBinary.substring(pos, pos+15), 2);
+                pos += 15;
+                System.out.println("Parsing length "+totalLength);
+                version += parseLengthPack(totalLength); // get the subpack sum
+                System.out.println("finished length "+totalLength);
+            }
+        }
+        return version;
+    }
 
     public static String hexToBin(String h) {
-        String b = Integer.toBinaryString(Integer.parseInt(h, 16));
+        String b = Long.toBinaryString(Long.parseLong(h, 16));
         String pading = "";
         while (pading.length() + b.length() < 4) {
             pading = pading.concat("0");
@@ -18,36 +81,13 @@ public class Day16_1{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
-        String rawBinary = "";
+        rawBinary = "";
         for (int i = 0; i < line.length(); i++) {
            rawBinary = rawBinary.concat(hexToBin(line.substring(i,i+1)));
         }
         System.out.println(rawBinary);
-        System.out.println("110100101111111000101000");
-        System.out.println(rawBinary.equals("110100101111111000101000"));
-        
-        int pos = 0;
-        int version = Integer.parseInt(rawBinary.substring(pos,pos+3), 2);
-        pos += 3;
-        int typeID  = Integer.parseInt(rawBinary.substring(pos,pos+3), 2);
-        pos += 3;
-        System.out.println(version);
-        System.out.println(typeID);
-
-        if (typeID == literalValue) {
-            String val = "";
-            while(rawBinary.charAt(pos) == '1') {
-                pos +=1;
-                val = val.concat(rawBinary.substring(pos, pos+4));
-                System.out.println(val);
-                pos +=4;
-            }
-            pos +=1;
-            val = val.concat(rawBinary.substring(pos, pos+4));
-            System.out.println(val);
-            pos +=4;
-
-            System.out.println(Integer.parseInt(val,2));
-        }
+        System.out.println("00111000000000000110111101000101001010010001001000000000");
+        //System.out.println(rawBinary.equals("110100101111111000101000"));
+        System.out.println("sum = " + parsePack());
     }
 }
