000e16c9e510e78254cc0a90f78c65a390ba6b49
Rafael Zuolo Coppini Lima <rafaelzcl@gmail.com>
2021-12-19 21:29:10 -0300

    changed implementation from tree to a linked list

--- Day18/Day18_1.java
+++ Day18/Day18_1.java
@@ -1,107 +1,72 @@
 import java.util.*;
 public class Day18_1 {
+    
+    static class Node {
+        int val;
+        int level;
+        Node next = null;
 
-    static class SnailNumber {
-        long a = -1;
-        long b = -1;
-        SnailNumber sa = null;
-        SnailNumber sb = null;
-        int nestNum = 0;
-        
-        public SnailNumber() {}
+        Node(int val, int level) {
+            this.val = val;
+            this.level = level;
+        }
+    }
+    static class Snail {
+        Node first = null;
+        Node current = null;
 
-        public SnailNumber(long a, long b) {
-            this.a = a;
-            this.b = b;
+        Snail(String s) {
+            int nest = -1;
+            int j = 0;
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if      (c == '[') nest++;
+                else if (c == ']') nest--;
+                else if (c == ',') continue;
+                else {
+                    if (first == null) {
+                        first = new Node(Integer.parseInt(s.substring(i,i+1)), nest);
+                        current = first;
+                    } else {
+                        Node temp = new Node(Integer.parseInt(s.substring(i,i+1)), nest);
+                        current.next = temp;
+                        current = temp;
+                    }
+                }
+            }
         }
-        public SnailNumber(SnailNumber sa, SnailNumber sb) {
-            this.sa = sa;
-            this.sb = sb;
-            sa.increaseNest();
-            sa.increaseNest();
-        }
-        public SnailNumber(SnailNumber sa, long b) {
-            this.sa = sa;
-            this.b = b;
-            sa.increaseNest();
-        }
-        public SnailNumber(long a, SnailNumber sb) {
-            this.a = a;
-            this.sb = sb;
-            sb.increaseNest();
-        }
-        public void increaseNest() {
-            nestNum++;
-            if (sa != null) sa.increaseNest();
-            if (sb != null) sb.increaseNest();
-        }
-        public void decreaseNest() {
-            nestNum--;
-            if (sa != null) sa.decreaseNest();
-            if (sb != null) sb.decreaseNest();
-        }
-
-        public String toString() {// inOrderTraversal
-            String s = "[";
-            if (sa == null) s = s.concat(a+"");
-            else s = s.concat(sa.toString());
-            s = s.concat(",");
-            if (sb== null) s = s.concat(b+"");
-            else s = s.concat(sb.toString());
-            s = s.concat("]");
+        public String toString() {
+            String s = "";
+            Node current = first;
+            while (current != null) {
+                s = s.concat("("+current.val +","+current.level+")");
+                current = current.next;
+            }
             return s;
         }
-/*        public void reduce() {
-            if (sa != null) {  
-                sa.reduce();
-            } else if (sb != null) {
-                sb.reduce();
-            } else if (nestNum == 4) { // explode
-                
-            } else if (a >= 10) {
-            
-            } else if (b >= 10) {
-            
+        public boolean explode() {
+            Node current = first;
+            Node previous = null;
+            while (current != null) {
+                if (current.level == 4) {
+                    if (previous != null) previous.val += current.val;
+                    if (current.next.next != null) current.next.next.val += current.next.val;
+                    current.val = 0;
+                    current.level -= 1;
+                    current.next = current.next.next;
+                    return true; 
+                }
+                previous = current;
+                current = current.next;
             }
-            
+            return false;
         }
-        */
     }
     
-    static SnailNumber parseSnail(String s) {
-        SnailNumber tempR = null;
-        SnailNumber tempL = null;
-        SnailNumber _return = new SnailNumber();
-        int commaIndex = 2;
-        if (s.charAt(1) != '[') _return.a = Integer.parseInt(s.substring(1,2));
-        else {
-            for (int i=1; i != 0; commaIndex++) {
-                if (s.charAt(commaIndex)=='[') i++;
-                if (s.charAt(commaIndex)==']') i--;
-            }
-            _return.sa = parseSnail(s.substring(1, commaIndex));
-        }
-        assert (s.charAt(commaIndex)==',');
-        if (s.charAt(commaIndex +1) != '[') _return.b = Integer.parseInt(s.substring(commaIndex+1, commaIndex+2));
-        else {
-            _return.sb = parseSnail(s.substring(commaIndex+1, s.length()));
-        }
-        return _return;
-    }
-    static SnailNumber add(SnailNumber sa, SnailNumber sb) {
-        SnailNumber sum = new SnailNumber(sa, sb);
-        return sum;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        sc.nextLine();
-        sc.nextLine();
-        sc.nextLine();
-        sc.nextLine();
-        sc.nextLine();
-        String s = sc.nextLine();
-        System.out.println(s);
-        System.out.println(parseSnail(s));
+        Snail sn = new Snail(sc.nextLine());
+        System.out.println(sn);
+        System.out.println(sn.explode() + " " + sn);
     }
 }
