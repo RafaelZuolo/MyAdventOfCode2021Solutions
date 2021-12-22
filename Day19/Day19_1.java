d2e302d44a660e0b472f8741f4c7592c0eb023f2
Rafael Zuolo Coppini Lima <rafaelzcl@gmail.com>
2021-12-22 11:47:04 -0300

    minor changes. Reading all beacons

--- Day19/Day19_1.java
+++ Day19/Day19_1.java
@@ -2,15 +2,22 @@ import java.util.*;
 public class Day19_1 {
     
     public static void main(String[] args) {
-        BeaconScan bc = new BeaconScan();
+
+        List<BeaconScan> scanList = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
-            String s = sc.nextLine();
-            if (s.equals("")) break;
-            if (s.charAt(1) == '-') continue;
-            bc.addBeacon(s);
+        while (sc.hasNextLine()) {
+            BeaconScan bc = new BeaconScan();
+            while(sc.hasNextLine()) {
+                String s = sc.nextLine();
+                if (s.equals("")) break;
+                if (s.charAt(1) == '-') continue;
+                bc.addBeacon(s);
+            }
+            scanList.add(bc);
         }
-        System.out.println(bc);
+        for (BeaconScan bc : scanList) {
+            System.out.println(bc);
+        }
     }
 }
 
