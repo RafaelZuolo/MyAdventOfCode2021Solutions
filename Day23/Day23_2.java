4123c7491b4f10b910a582200f9d36ef10cf5334
RafaelZuolo <rafaelzcl@gmail.com>
2022-04-17 23:04:46 -0300

    Done half the data structure. Part 1 was done by hand

---
+++ Day23/Day23_2.java
@@ -0,0 +1,167 @@
+import java.util.*;
+public class Day23_2 {
+   	static class State {
+		char[][] hole;
+		char[] hall;
+		public State(char[][] ho, char[] ha) {
+			hole = ho;
+			hall = ha;
+		}
+
+		public State clone() {
+			char[][] cloneHole = new char[4][4];
+			char[] cloneHall = Arrays.copyOf(hall, hall.length);
+			for (int i = 0; i < 4; i++) {
+				cloneHole[i] = Arrays.copyOf(hole[i], hole[i].length);
+			}
+			return new State(cloneHole, cloneHall);
+		}
+		
+		public int hashCode() {
+			int hash = Arrays.hashCode(hall);
+			for (int i = 0; i < hole.length; i++) {
+				hash = hash ^ Arrays.hashCode(hole[i]);
+			}
+			return hash;
+		}
+
+		public String toString() {		
+			Formatter f = new Formatter();
+			String out = f.format("#############\n#%c%c%c%c%c%c%c%c%c%c%c#\n###%c#%c#%c#%c###\n  #%c#%c#%c#%c#\n  #%c#%c#%c#%c#\n  #%c#%c#%c#%c#\n  #########", hall[0],hall[1],hall[2],hall[3],hall[4],hall[5],hall[6],hall[7],hall[8],hall[9],hall[10],hole[0][0],hole[1][0],hole[2][0],hole[3][0],hole[0][1],hole[1][1],hole[2][1],hole[3][1],hole[0][2],hole[1][2],hole[2][2],hole[3][2],hole[0][3],hole[1][3],hole[2][3],hole[3][3]).toString();
+			return out;
+		}
+		
+		public boolean equals(State that) {
+			if (!Arrays.equals(this.hall, that.hall))
+				return false;
+			for (int i = 0; i < hole.length; i++) {
+				if (!Arrays.equals(this.hole[i], that.hole[i]))
+					return false;
+			}
+			return true;
+		}
+		
+		public static int lastPositionEmpty(char[] array) {  // return the last address of the array with the char '.'
+			for (int i = 0; i < array.length; i++) {
+				if (array[i] != '.') return (i-1);
+			}
+			return (array.length-1);
+		}
+		
+		public static boolean holeIsPure(char amphipod, char[] amphiHole) { // check if hole only have one specific type of amphipode
+			for (int i = 0; i < amphiHole.length; i++) {
+				if (amphiHole[i] != amphipod && amphiHole[i] != '.')
+					return false;
+			}
+			return true;
+		}
+
+		public static boolean hallIsClear(int start, int end, char[] currentHall) {
+			int increment = 1;
+			if (start < end) increment = -1;
+			for (int i = end; i!= start; i += increment) {
+				if (currentHall[i] != '.') return false;
+			}
+			return true;
+		}
+
+		//
+		// brace yourself for convoluted if-else chained spaghetti
+		//
+		public Map<State, Integer> adj() { // compute the adjacent states and the cost to reach them
+			Map<State, Integer> adj = new HashMap<>();
+			// hall to hole movements
+			for (int i = 0; i < this.hall.length; i++) {
+				if (hall[i] == '.') continue;
+				// case A. Address of hole of 'A' is [0], and hall entrance is [2] 
+				if (hall[i] == 'A' && holeIsPure('A', hole[0]) && hallIsClear(i, 2, this.hall)) {
+					int holePosition = lastPositionEmpty(hole[0]);
+					int cost = Math.abs(i - 2) + holePosition + 1;
+					State step = this.clone();
+					step.hall[i] = '.';
+					step.hole[0][holePosition] = 'A';
+					adj.put(step, cost);
+				} 
+				// case B. Address oh hole of 'B' is [1], and hall entrance is [4]
+				if (hall[i] == 'B'  && holeIsPure('B' , hole[1]) && hallIsClear(i, 4, this.hall)) {
+					int holePosition = lastPositionEmpty(hole[1]);
+					int cost = 10*(Math.abs(i - 4) + holePosition + 1);
+					State step = this.clone();
+					step.hall[i] = '.';
+					step.hole[1][holePosition] = 'B';
+					adj.put(step, cost);
+				}
+				// case C. Address oh hole of 'C' is [2], and hall entrance is [6]
+				if (hall[i] == 'C' && holeIsPure('C', hole[2]) && hallIsClear(i, 6, this.hall)) {
+					int holePosition = lastPositionEmpty(hole[2]);
+					int cost = 100*(Math.abs(i - 6) + holePosition + 1);
+					State step = this.clone();
+					step.hall[i] = '.';
+					step.hole[2][holePosition] = 'C';
+					adj.put(step, cost);
+				}
+				// case D. Address oh hole of 'D' is [3], and hall entrance is [8]
+				if (hall[i] == 'D' && holeIsPure('D', hole[3]) && hallIsClear(i, 8, this.hall)) {
+					int holePosition = lastPositionEmpty(hole[3]);
+					int cost = 1000*(Math.abs(i - 8) + holePosition + 1);
+					State step = this.clone();
+					step.hall[i] = '.';
+					step.hole[3][holePosition] = 'D';
+					adj.put(step, cost);
+				}
+			}
+            
+            // hole to hall movements
+            char[] originalHole = new char[]{'A', 'B', 'C', 'D'}; // easy  map of index to hole type
+            for (int i = 0; i < hole.length; i++) {
+                if (holeIsPure(originalHole[i], hole[i]) && lastPositionEmpty(hole[i]) < hole[i].length-1) // nothing will leave this hole
+                    continue;
+                int position = lastPositionEmpty(hole[i]) + 1; // 
+                assert position < hole[i].length;    
+                int hallStartPos = (i+1)*2;   // position in the hall
+                for (int j = 0; j < hall.length; j++) {
+                    int cost = position + 1; // cost of moving out of the hole
+                    int multiplicator = 1;
+                    char amphipod = hole[i][position];
+                    assert amphipod != '.';
+                    if ( == 'B' )
+                    if (j==2 || j==4 || j==6 || j==8) 
+                        continue; // forbidden hall positions
+                    if (hallIsClear(hallStartPos, j, this.hall)) {
+                        State step = this.clone();
+                        assert hall[j] == '.';
+                        step.hall[j] = step.hole[i][position];
+                        step.hole[i][position] = '.';
+                        adj.put(step, )
+                    }
+                }
+            }
+			return adj;
+		}
+	}
+
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		char[][] startHole = new char[4][4];
+		char[]	 startHall = new char[11];
+		sc.nextLine();
+		String firstLine = sc.nextLine(); 		// first two lines dont contain Amphipod
+		for (int i = 0; i < 11; i++) 
+			startHall[i] = firstLine.charAt(i+1);
+		for (int i = 0; i < 4; i++) {
+			String line = sc.nextLine();
+			for (int j = 0; j < 4; j++) {
+				startHole[j][i] = line.charAt(2*j+3);
+			}
+		}
+		State start = new State(startHole, startHall);
+		System.out.println(start);
+
+		Map<State, Integer> Visited = new HashMap<>();
+		Map<State, Integer> Hook = new HashMap<>();
+		Hook.put(start, 0);
+		Map<State, Integer> test = start.adj();
+		for (State s : test.keySet())
+			System.out.println(s);
+	}
+}
